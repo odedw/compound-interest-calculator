73e2064d25c94f6ce81d4d5e8b9f8ed7ec254816
Oded Welgreen <oded.welgreen@gmail.com>
2020-03-05 21:57:13 -0600

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #282c34;
+  height: 100%;
+`;
+function App() {
+  return <Container>Hi</Container>;
+}
+
+export default App;
