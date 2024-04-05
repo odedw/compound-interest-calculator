35d6f517ef2e442035677f220659b6ed9eca92fc
Oded Welgreen <oded.welgreen@gmail.com>
2024-04-05 08:00:56 -0500

    add build

---
+++ build/precache-manifest.1eac00ebe3a6483ea51ded735bbbfb7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7257562e9fb30f597a49c01e5599ea7",
+    "url": "/compound-interest-calculator/index.html"
+  },
+  {
+    "revision": "89ac269328154ec785f5",
+    "url": "/compound-interest-calculator/static/css/2.4c0dc63d.chunk.css"
+  },
+  {
+    "revision": "f4d6a05e2aa5ae1cf9b7",
+    "url": "/compound-interest-calculator/static/css/main.c0201c46.chunk.css"
+  },
+  {
+    "revision": "89ac269328154ec785f5",
+    "url": "/compound-interest-calculator/static/js/2.3ac33d59.chunk.js"
+  },
+  {
+    "revision": "9b528eef403c0dfc0b7fd9ba42efbb30",
+    "url": "/compound-interest-calculator/static/js/2.3ac33d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d6a05e2aa5ae1cf9b7",
+    "url": "/compound-interest-calculator/static/js/main.6c06fe36.chunk.js"
+  },
+  {
+    "revision": "529b2549394542b40af7",
+    "url": "/compound-interest-calculator/static/js/runtime-main.32a005fc.js"
+  }
+]);
